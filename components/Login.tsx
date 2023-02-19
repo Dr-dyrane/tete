86858a14fb0d8ea2c062e3d1bac807cccd25fa3f
Dr-dyrane <halodyrane@gmail.com>
2023-02-19 17:31:35 +0100

    Login

--- components/Login.tsx
+++ components/Login.tsx
@@ -5,8 +5,14 @@ import Image from "next/image";
 
 export default function Login() {
   return (
-    <div className="bg-purple-900 text-white h-screen items-center justify-center flex">
-      <Image src="/images/tete.png" width={300} height={300} alt="logo" />
+    <div
+      className="bg-purple-900 text-white text-center h-screen 
+    items-center justify-center space-y-8 flex flex-col"
+    >
+      <Image src="/images/tete.png" width={150} height={150} alt="logo" />
+      <button className="font-bold text-3xl animate-pulse">
+        Sign In to use TêteBöT
+      </button>
     </div>
   );
 }
