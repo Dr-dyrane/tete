ef7a1cd2256703a89006cae256cd81b76ed057cd
Dr-dyrane <halodyrane@gmail.com>
2023-02-19 17:16:32 +0100

    tete.png

--- components/Login.tsx
+++ components/Login.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 export default function Login() {
   return (
     <div className="bg-purple-900 text-white h-screen items-center justify-center flex">
-      <Image src="/public/favicon.ico" width={300} height={300} alt="logo" />
+      <Image src="/images/tete.png" width={300} height={300} alt="logo" />
     </div>
   );
 }
